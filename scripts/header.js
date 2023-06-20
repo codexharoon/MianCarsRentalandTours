73d32fce9eda698e88c35b1f0779732eb83188fd
Muhammad Haroon <contactwithharoon@gmail.com>
2023-06-20 13:30:48 +0500

    code until two nav bar

---
+++ scripts/header.js
@@ -0,0 +1,10 @@
+
+
+document.querySelector('.js-menu-btn').addEventListener('click',()=>{
+    document.querySelector('.sidebar-menu').classList.add('show-sidebar-menu');
+    document.querySelector('.sidebar-menu').classList.remove('hide-sidebar-menu');
+});
+
+document.querySelector('.js-close-btn').addEventListener('click',()=>{
+    document.querySelector('.sidebar-menu').classList.add('hide-sidebar-menu');
+});
