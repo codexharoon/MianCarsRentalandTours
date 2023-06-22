955b12c5dc2df26c4c75de7e3805fcba5576e6e4
Muhammad Haroon <contactwithharoon@gmail.com>
2023-06-22 12:18:43 +0500

    addedd our services cards

--- scripts/header.js
+++ scripts/header.js
@@ -8,3 +8,11 @@ document.querySelector('.js-menu-btn').addEventListener('click',()=>{
 document.querySelector('.js-close-btn').addEventListener('click',()=>{
     document.querySelector('.sidebar-menu').classList.add('hide-sidebar-menu');
 });
+
+document.querySelectorAll('.menu').forEach(
+    (menuBtn)=>{
+        menuBtn.addEventListener('click',()=>{
+            document.querySelector('.sidebar-menu').classList.add('hide-sidebar-menu');
+        });
+    }
+);
