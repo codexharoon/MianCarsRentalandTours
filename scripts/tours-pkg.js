f4233d227a882f80fd751b73d87dd8c7f285e60d
Muhammad Haroon <contactwithharoon@gmail.com>
2023-07-14 18:45:21 +0500

    add tours pakages details plus change contact info

--- scripts/tours-pkg.js
+++ scripts/tours-pkg.js
@@ -1,42 +1,118 @@
 
 const tableHead = {
         h1:'Tours Pakages',
-        h2:'No. of Days / Nights',
-        h3:'Price',
+        h2:'Per Head ( With Group )',
+        h3:'Lump Sum ( Family Trip )',
 };
 
 
 const toursPkgDetails = [
     {
-        title:'Naran Kaghan Tours Pakages',
+        title:'Naran Kaghan Tours Pakages (For More Details Contact!)',
 
-        r11:'Cost-Effective Naran Kaghan Tour Package',
-        r12:'4 Days Tour',
+        r11:'Cost-Effective ( 3 Days )',
+        r12:'Rs. 14,000',
         r13:'Rs. 75,000',
 
-        r21:'Naran  & Neelum Combo Plan for Couples',
-        r22:'6 – 7 Days',
-        r23:'Rs. 150,000',
-
-        r31:'N/A',
-        r32:'N/A',
-        r33:'N/A',
+        r21:'Naran  & Neelum Combo Plan for Couples (6 - 7 Days)',
+        r22:'Rs. 85,000',
+        r23:'Rs. 85,000',
 
     },
     {
-        title:'Hunza Valley Tours Pakages',
+        title:'Hunza Valley Tours Pakages (For More Details Contact!)',
 
-        r11:'Hunza Valley Deluxe Tour',
-        r12:'7 Days / 6 Nights',
-        r13:'Starting From 175,000 PKR',
+        r11:'05 Days Hunza',
+        r12:'Rs. 24,500/-',
+        r13:'N/A',
 
-        r21:'Hunza Valley Standard Tour',
-        r22:'7 Days / 6 Nights',
-        r23:'Starting From 140,000 PKR',
+        r21:'07 Days Hunza',
+        r22:'Rs. 29,500/-',
+        r23:'N/A',
 
-        r31:'Hunza Naran 10 Days Tour',
-        r32:'10 Days / 9 Nights',
-        r33:'Starting From 230,000 PKR',
+    },
+    {
+        title:'Swat Valley Tours Pakages (For More Details Contact!)',
+
+        r11:'03, Days Swat',
+        r12:'Rs. 13,500/-',
+        r13:'N/A',
+
+        r21:'N/A',
+        r22:'N/A',
+        r23:'N/A',
+
+    },
+    {
+        title:'Murree Tours Pakages (For More Details Contact!)',
+
+        r11:'1,2,3 Days Murree Galyat',
+        r12:'Rs. 10,500/-',
+        r13:'N/A',
+
+        r21:'N/A',
+        r22:'N/A',
+        r23:'N/A',
+
+    },
+    {
+        title:'Chitral Valley Tours Pakages (For More Details Contact!)',
+
+        r11:'05 Days Chitral',
+        r12:'Rs. 22,500/-',
+        r13:'N/A',
+
+        r21:'N/A',
+        r22:'N/A',
+        r23:'N/A',
+
+    },
+    {
+        title:'Skardu Valley Pakages (For More Details Contact!)',
+
+        r11:'08 Days Skardu',
+        r12:'Rs. 32,500/-',
+        r13:'N/A',
+
+        r21:'10 Days Hunza+Skardu',
+        r22:'Rs. 38,500/-',
+        r23:'N/A',
+
+    },
+    {
+        title:'Kashmir Tours Pakages (For More Details Contact!)',
+
+        r11:'03, Days Kashmir',
+        r12:'Rs. 14,000/-',
+        r13:'N/A',
+
+        r21:'N/A',
+        r22:'N/A',
+        r23:'N/A',
+
+    },
+    {
+        title:'Kumrat Tours Pakages (For More Details Contact!)',
+
+        r11:'04, Days Kumrat ',
+        r12:'Rs. 17,500/-',
+        r13:'N/A',
+
+        r21:'N/A',
+        r22:'N/A',
+        r23:'N/A',
+
+    },
+    {
+        title:'Custom Pakistan Tours Pakages (For More Details Contact!)',
+
+        r11:'01 Day Umbrella waterfall+ Khanpur Dam',
+        r12:'Rs. 7,000/-',
+        r13:'N/A',
+
+        r21:'02 Days Shogran Siri Pay',
+        r22:'Rs. 9,000/-',
+        r23:'N/A',
 
     },
 ];
@@ -72,11 +148,6 @@ toursPkgDetails.forEach((tourPkg)=>{
                         <td>${tourPkg.r23}</td>
                     </tr>
 
-                    <tr>
-                        <td>${tourPkg.r31}</td>
-                        <td>${tourPkg.r32}</td>
-                        <td>${tourPkg.r33}</td>
-                    </tr>
                 </tbody>
             </table>
 
--- scripts/tours.js
+++ scripts/tours.js
@@ -1,52 +1,52 @@
-
+const phone = '0300 9049602';
 const toursCardsInfo = [
     {
         img: `IMGs/Tours/naran.jpg`,
         title: 'Naran Kaghan Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
     {
         img: `IMGs/Tours/hunza.jpg`,
         title: 'Hunza Valley Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
     {
         img: `IMGs/Tours/swat.jpg`,
         title: 'Swat Valley Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
     {
         img: `IMGs/Tours/muree.jpg`,
         title: 'Murree Galyat Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
     {
         img: `IMGs/Tours/chitral.jpg`,
         title: 'Chitral Valley Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
     {
         img: `IMGs/Tours/skardu.jpg`,
         title: 'Skardu Valley Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
     {
         img: `IMGs/Tours/neelum.jpg`,
         title: 'Neelum Valley Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
     {
         img: `IMGs/Tours/custom.jpg`,
         title: 'Custom Pakistan Tour',
         pkg: 'Premium Tour Pakages',
-        phone: '0300 4218333'
+        phone
     },
 ];
 
